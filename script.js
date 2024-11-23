fea9022f0c597396811a16d959d8e333afcfc3e6
Cyprien <126987054+cypripricheche@users.noreply.github.com>
2024-11-23 21:05:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ menu_toggle.onclick = function() {
     menu_toggle.classList.toggle('active');
     menu.classList.toggle('responsive');
 }
+
+
+// ============================================================================================================================
+// OPTION - CLASH OF CLAN
+// ============================================================================================================================
+
+document.querySelectorAll('.tab-button').forEach(button => {
+    button.addEventListener('click', () => {
+        // Remove active class from all buttons and contents
+        document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
+        document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
+
+        // Add active class to the clicked button and corresponding content
+        button.classList.add('active');
+        const target = button.getAttribute('data-target');
+        document.getElementById(target).classList.add('active');
+    });
+});
