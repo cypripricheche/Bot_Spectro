b22e0d8c93551536ae78c79f0432157c667fe6bd
Cyprien <126987054+cypripricheche@users.noreply.github.com>
2024-12-17 23:50:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                     card.innerHTML = `
                         <div class="clanImage">
-                            <img src="<img src="Image/Autre/ClashOfClans.png" alt="Clan Image">
+                            <img src="Image/Autre/ClashOfClans.png" alt="Clan Image">
                             <div class="clanDate">
                                 <span class="icon">🕒</span> ${formatTimeAgo(publicationDate)}
                             </div>
